4824bb5555fd5c9e896455451a67327dc16d3872
CaptoshkaLA <captoshkala@gmail.com>
2024-06-12 23:34:21 +0300

    Reduce min password to 5 chars

--- src/components/Pages/Login.tsx
+++ src/components/Pages/Login.tsx
@@ -17,7 +17,7 @@ interface LoginFormData {
 
 const schema = yup.object().shape({
   email: yup.string().email('Invalid email').required('Email is required'),
-  password: yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+  password: yup.string().min(5, 'Password must be at least 5 characters').required('Password is required'),
 });
 
 const Login: React.FC = () => {
