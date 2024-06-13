14d31e39b4b995291904a41fed2b7e3232134316
Anton Lisovskiy <alisovskiy@science-soft.ru>
2024-06-13 10:56:36 +0300

    Added two new cards to the home page

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -31,6 +31,26 @@ const Home: React.FC = () => {
             />
           </Card>
         </Grid>
+        <Grid item xs={6}>
+          <Card sx={{ maxWidth: '100%', height: '100%' }}>
+            <CardMedia
+              component="img"
+              height="auto"
+              image="https://sun1-27.userapi.com/impf/QOOReQmDv7qLNMJgEOLPQSezfTyC4VHPCCwBrg/qtSYj4yfZ-w.jpg?quality=95&as=32x16,48x24,72x36,108x53,160x79,240x118,360x178,480x237,540x267,640x316,720x355,1080x533,1280x632,1440x711,1920x948&sign=4d375d5903d4ff3601c14c6cb85dc638&from=bu&u=MwbAxziC-DGvspWRgFi-eI7nnzhquYhvB48sCDbzEN0&cs=604x298"
+              alt="Restaurant DSR 3"
+            />
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card sx={{ maxWidth: '100%', height: '100%' }}>
+            <CardMedia
+              component="img"
+              height="auto"
+              image="https://sun9-64.userapi.com/impf/W0gOj5r9Jf5-S-7nfxzPHQC3W04BXjbhcqLejA/mcsAe6IMHQ0.jpg?quality=95&as=32x16,48x24,72x36,108x53,160x79,240x118,360x178,480x237,540x267,640x316,720x355,1080x533,1280x632,1440x711,1920x948&sign=3f48efc6bdd02bc65b81965fc4daa4e9&from=bu&u=NxBigkNX0OrOCE_DZqN3cTi5BlRhSzhG_7L9G6q7JQg&cs=604x298"
+              alt="Restaurant DSR 4"
+            />
+          </Card>
+        </Grid>
       </Grid>
     </Container>
   );
